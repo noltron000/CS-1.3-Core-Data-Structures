7f26f99edbcd950287f0f8caa20e850c9fd6563c
Nolan Kovacik <noltron000@gmail.com>
2019-04-02 19:17:39 -0700

    💯 encode decode tests pass

--- source/bases.py
+++ source/bases.py
@@ -12,37 +12,59 @@ import string
 
 # a GRAPHEME is the smallest meaningful contrastive unit in a writing system
 # it represents a number whose maximum is determined by whatever base system being used - 1
-GRAPHEMES = string.digits + string.ascii_uppercase
+GRAPHEMES = string.digits + string.ascii_lowercase + string.ascii_uppercase
 
 def decode(digits, base):
 	"""Decode given digits in given base to number in base 10.
 	digits: str -- string representation of number (in given base)
 	base: int -- base of given number
 	return: int -- integer representation of number (in base 10)"""
+
+	# we have to reverse digits to iterate correctly!
+	digits = "".join(reversed(digits))
+
+	print(f'---DECODE--- digs: {digits} base: {base}')
 	# Handle up to base 36 [0-9A-Z]
 	assert 2 <= base <= 36, f'base is out of range: {base}'
-	# TODO: Encode number in any base
+	# Handle unsigned numbers only for now
+	# assert number >= 0, f'number is negative: {number}'
+	# Iterate through digits string
+	# i: degree iterator
+	i = len(digits) - 1
+	print(f'digits {i + 1}')
+	# result: return keeper
+	result = 0
+
+	while i >= 0 :
+		# value is a number < base, representing a digit's value in base-10
+		# it is found by looking up its index in the graphemes string
+		# hex: | A  | F  | 4  | 9  |
+		# val: | 11 | 15 | 04 | 09 |
+		value = GRAPHEMES.index(digits[i])*base**i
+		print(f'i: {i} -- digits[i]: {digits[i]} -- value: {value}')
+		result += value
+		i -= 1
+
+	return result
 
 def encode(number, base):
 	"""Encode given number in base 10 to digits in given base.
 	number: int -- integer representation of number (in base 10)
 	base: int -- base to convert to
 	return: str -- string representation of number (in given base)"""
+	### print(f'\n---encode function--- num:{number} base:{base}')
 	# Handle up to base 36 [0-9A-Z]
-	print(f'\n---encode function--- num:{number} base:{base}')
 	assert 2 <= base <= 36, f'base is out of range: {base}'
 	# Handle unsigned numbers only for now
 	assert number >= 0, f'number is negative: {number}'
-	# TODO: Encode number in any base
-
 	# Find out first n where (base**degree > number)
 	# n+1 turns out to be number of digits needed
 	degree = 0
 	while base**degree <= number:
-		print(f'@degree {degree}: {base**degree} is less than or equal to {number}')
+		### print(f'@degree {degree}: {base**degree} is less than or equal to {number}')
 		degree += 1
-	print(f'@degree {degree}: {base**degree} is more than {number}')
-	print(f'There are {degree} base-{base} digits the answer\n')
+	### print(f'@degree {degree}: {base**degree} is more than {number}')
+	### print(f'There are {degree} base-{base} digits the answer\n')
 
 	# i: degree iterator
 	i = degree - 1
@@ -67,7 +89,7 @@ def encode(number, base):
 
 	# make sure function didn't explode before returning
 	assert remain == 0, f'function didn\'t return 0; returned {remain}'
-
+	# return resulting string
 	return result
 
 def convert(digits, baseX, baseY):
@@ -77,12 +99,11 @@ def convert(digits, baseX, baseY):
 	baseY: int -- base to convert to
 	number10: int -- digits number in base 10
 	return: str -- string representation of number (in baseY)"""
-	# Handle up to base 36 [0-9A-Z]
+	#  Handle up to base 36 [0-9A-Z]
 	assert 2 <= baseX <= 36, 'baseX is out of range: {}'.format(baseX)
 	assert 2 <= baseY <= 36, 'baseY is out of range: {}'.format(baseY)
 	number10 = decode(digits, baseX)
-	# return encode(number10, baseY)
-	return encode(33, baseY)
+	return encode(number10, baseY)
 
 def main():
 	"""Read command-line arguments and convert given digits between bases."""
