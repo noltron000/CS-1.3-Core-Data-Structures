fa0df56b8ade1b1300595b1ce48ff02fe1abe7c7
Nolan Kovacik <noltron000@gmail.com>
2019-04-23 22:01:25 -0700

    setting set setup

---
+++ source/set.py
@@ -0,0 +1,73 @@
+#!python
+
+from hasthable import HashTable
+
+class Set():
+	'''
+	[NOTE] DOCSTRING
+	'''
+
+
+	def __init__(this, data=None):
+		'''
+		[NOTE] DOCSTRING
+		'''
+		# [TODO] COMPLETE
+		this.size = 0
+		this.table = HashTable()
+
+
+	def contains(this, item):
+		'''
+		[NOTE] DOCSTRING
+		'''
+		# [TODO] COMPLETE
+		pass
+
+
+	def add(this, item):
+		'''
+		[NOTE] DOCSTRING
+		'''
+		# [TODO] COMPLETE
+		pass
+
+
+	def remove(this, item):
+		'''
+		[NOTE] DOCSTRING
+		'''
+		# [TODO] COMPLETE
+		pass
+
+
+	def union(this, that):
+		'''
+		[NOTE] DOCSTRING
+		'''
+		# [TODO] COMPLETE
+		pass
+
+
+	def intersection(this, that):
+		'''
+		[NOTE] DOCSTRING
+		'''
+		# [TODO] COMPLETE
+		pass
+
+
+	def difference(this, that):
+		'''
+		[NOTE] DOCSTRING
+		'''
+		# [TODO] COMPLETE
+		pass
+
+
+	def is_subset(this, that):
+		'''
+		[NOTE] DOCSTRING
+		'''
+		# [TODO] COMPLETE
+		pass
