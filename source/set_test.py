dd7165dced734516786ff5f24f88f85ccd03436e
Nolan Kovacik <noltron000@gmail.com>
2019-05-01 16:29:14 -0700

    adding set tests

--- source/set_test.py
+++ source/set_test.py
@@ -1,1 +1,17 @@
 #!python
+
+import unittest
+from set import Set
+from hashtable import HashTable
+
+class SetTest(unittest.TestCase):
+	def test_init(self):
+		simple_array = [1,2,3,'a','b','c']
+
+		set_one = Set()
+		set_two = Set(simple_array)
+		assert(set_one.table.keys() == [])
+		set_two.table.keys()
+
+if __name__ == '__main__':
+	unittest.main()
